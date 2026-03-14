c6e36a9432b7cb88d36e34a7a5c96f4e87892e8c
liangnijian <3190776046@qq.com>
2026-03-14 14:34:31 +0800

    Update main.cmd

--- projects/99-wim/main.cmd
+++ projects/99-wim/main.cmd
@@ -1,2 +1,2 @@
 echo 打包 wim
-wimlib-imagex.exe capture "%X%" "%Z%\WinPE.wim" "WindowsPE" --boot --compress=lzx --rebuild
+wimlib-imagex.exe capture "%X%" "%Z%\boot.wim" "WindowsPE" --boot --compress=lzx --rebuild
