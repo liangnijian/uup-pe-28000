e4b05d9e53089bfadefc319b777b8734aebd1ddf
liangnijian <3190776046@qq.com>
2026-03-17 14:08:09 +0800

    new

--- auto-run.cmd
+++ auto-run.cmd
@@ -2,7 +2,11 @@
 
 tzutil /s "China Standard Time"
 
-
+for /f %%i in ('powershell -command "& { $today=Get-Date; $firstOfMonth=Get-Date -Year $today.Year -Month $today.Month -Day 1; $firstWednesday=$firstOfMonth; while ($firstWednesday.DayOfWeek -ne 'Wednesday') { $firstWednesday=$firstWednesday.AddDays(1) }; $secondWednesday=$firstWednesday.AddDays(7); $today.Date -eq $secondWednesday.Date }"') do set "result=%%i"
+if "%result%"=="False" (
+	echo 不是本月的第二个星期三
+	exit /b 1
+)
 
 if "%~1"=="uup" call uup-iso.cmd
 
